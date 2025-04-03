d382f672273c5e14da74270bda3343f59fd20553
astral-up97 <alaltarejos@up.edu.ph>
2025-04-03 14:04:37 +0800

    Update iris_app.py

--- iris_app.py
+++ iris_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+from sklearn.preprocessing import StandardScaler
 
 with open("iris_svm_model.pkl","rb") as f:
     model = pickle.load(f)
@@ -16,6 +17,9 @@ sepal_length = st.slider("Sepal lgnth (cm)", 4.0, 8.0, 5.8)
 sepal_width = st.slider("Sepal width (cm)", 2.0, 5.0, 3.0)
 petal_length = st.slider("Petal legnth (cm)", 1.0, 7.0, 4.2)
 petal_width = st.slider("Petal width (cm)", 0.1, 3.0, 1.3)
+
+scaler=StandardScaler()
+
 
 input_data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
 input_scaled = scaler.transform(input_data)
